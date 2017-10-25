fd7fdc754daf437c10e9b38e4572158d54c164fc
danangyudhatama <danang.yudhatama@gmail.com>
2017-10-26 07:05:51 +0800

    new branch commit

---
+++ js/textBahasaDaerah.js
@@ -0,0 +1,11 @@
+class BahasaDaerah {
+	constructor(text, asalDaerah) {
+		this.text = text;
+		this.asalDaerah = asalDaerah;
+	}
+}
+
+
+var dataArray = [
+let sunda = new BahasaDaerah("Koding téh hadé tapi tong ka leuleuwihan","Sunda")];
+
