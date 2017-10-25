29a2897a62941414414278985c67885a3c20e9fc
danangyudhatama <danang.yudhatama@gmail.com>
2017-10-25 09:40:28 +0800

    refactor the code, tambah bahasa pemrograman untuk menampilkan bahasa daerah

--- js/app.js
+++ js/app.js
@@ -1,32 +1,28 @@
-$(document).ready(function () {
+//$(document).ready(function () {
     //rotation speed and timer
-    var speed = 1500;
-    
-    var run = setInterval(rotate, speed);
+    var speed = 3500;
     var slides = $('.slide');
     var container = $('#slides ul');
-    var elm = container.find(':first-child').prop("tagName");
     var item_width = container.width();
     slides.width(item_width); //set the slides to the correct pixel width
     container.parent().width(item_width);
     container.width(slides.length * item_width); //set the slides container to the correct total width
-    container.find(elm + ':first').before(container.find(elm + ':last'));
-    resetSlides();
+    setInterval(rotate, speed);
     
     function resetSlides() {
         //and adjust the container so current is in the frame
         container.css({
-            'left': -1 * item_width
+            'left': 0
         });
     }
 
     function rotate() {
     container.stop().animate({
-        'left': item_width * -2
-        }, 250, function () {
-        container.find(elm + ':last').after(container.find(elm + ':first'));
+        'left': item_width * -1
+        }, 1500, function () {
+        container.find('li:last').after(container.find('li:first'));
         resetSlides();
     });
     }
     
-});
+//});
