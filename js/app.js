9b206e79028cf7a211ac1c91374c739a5416ccf3
danangyudhatama <danang.yudhatama@gmail.com>
2017-10-30 10:26:43 +0800

    change to app.js

--- js/app.js
+++ js/app.js
@@ -10,17 +10,17 @@ for (var i = 0; i < arrayFullText.length; i++) {
 
 
 //rotation speed and timer
-var speed = 1500;
+var speed = 1500; // berapa lama slide akan ditampilkan dalam milisekon
 var slides = $('.slide');
-var container = $('#slides ul');
+var container = $('#slides ul'); 
 var item_width = container.width();
-slides.width(item_width); //set the slides to the correct pixel width
+slides.width(item_width); // menentukan lebar kontainer agar mulus animasinya
 container.parent().width(item_width);
-container.width(slides.length * item_width); //set the slides container to the correct total width
-setInterval(rotate, speed);
+container.width(slides.length * item_width); // menentukan lebar slide kontainer agar semua slidenya bisa masuk
+setInterval(rotate, speed); // setiap 1,5 detik maka slide akan digeser
 
 function resetSlides() {
-    //and adjust the container so current is in the frame
+    //reset posisi left property container ke nol lagi
     container.css({
         'left': 0
     });
@@ -28,9 +28,9 @@ function resetSlides() {
 
 function rotate() {
 container.stop().animate({
-    'left': item_width * -1
-    }, 500, function () {
-    container.find('li:last').after(container.find('li:first'));
-    resetSlides();
+    'left': item_width * -1 // geser kontainer ke kiri sebesar lebar slide
+    }, 500, function () {  // waktu animasi 500 ms
+    container.find('li:last').after(container.find('li:first')); //pindahkan slide pertama ("li:first") setelah slide terakhir("li:last")
+    resetSlides(); //reset posisi container
 });
 }
