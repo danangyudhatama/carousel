23d5ab909dec58f7002d168817ef3ecbd6af07c2
danangyudhatama <danang.yudhatama@gmail.com>
2017-10-30 11:43:26 +0800

    need to be fixed

--- js/app.js
+++ js/app.js
@@ -1,28 +1,26 @@
-//$(document).ready(function () {
-    //rotation speed and timer
-    var speed = 3000;
-    var slides = $('.slide');
-    var container = $('#slides ul');
-    var item_width = container.width();
-    slides.width(item_width); //set the slides to the correct pixel width
-    container.parent().width(item_width);
-    container.width(slides.length * item_width); //set the slides container to the correct total width
-    setInterval(rotate, speed);
-    
-    function resetSlides() {
-        //and adjust the container so current is in the frame
-        container.css({
-            'left': 0
-        });
-    }
 
-    function rotate() {
-    container.stop().animate({
-        'left': item_width * -1
-        }, 500, function () {
-        container.find('li:last').after(container.find('li:first'));
-        resetSlides();
+//rotation speed and timer
+var speed = 3000;
+var slides = $('.slide');
+var container = $('#slides ul');
+var item_width = container.width();
+slides.width(item_width); //set the slides to the correct pixel width
+container.parent().width(item_width);
+container.width(slides.length * item_width); //set the slides container to the correct total width
+setInterval(rotate, speed);
+
+function resetSlides() {
+    //and adjust the container so current is in the frame
+    container.css({
+        'left': 0
     });
-    }
-    
-//});
+}
+
+function rotate() {
+container.stop().animate({
+    'left': item_width * -1
+    }, 500, function () {
+    container.find('li:last').after(container.find('li:first'));
+    resetSlides();
+});
+}
